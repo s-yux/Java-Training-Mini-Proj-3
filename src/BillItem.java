e7c494e45b3949cc124caf1fa6078274bf5f1d72
s-yux <sylomrouza24@gmail.com>
2022-04-02 17:34:50 +0800

    Extracted Java Training Mini-Proj 3

---
+++ src/BillItem.java
@@ -0,0 +1,14 @@
+public class BillItem {
+
+    int itemID;
+    String itemName;
+    int quantity;
+    double totalPrice;
+
+    BillItem(int choice, String itemName, int quantity, double totalPrice){
+        this.itemID = choice;
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+}
---
+++ src/MenuItem.java
@@ -0,0 +1,11 @@
+public class MenuItem {
+    int itemID;
+    String itemName;
+    double itemPrice;
+
+    MenuItem(int itemID, String itemName, double itemPrice) {
+        this.itemID = itemID;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+    }
+}
